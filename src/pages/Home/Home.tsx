902b49970954620c854d95f380a318b8048fb7bd
canting <cantingz@uci.edu>
2024-05-25 17:15:40 -0700

    add cancel button, remove password for student login, fix the issue of void name login

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { yellow } from '@mui/material/colors';
 import './Home.css';
 import React, { useState, useEffect } from 'react';
 
@@ -9,32 +10,39 @@ interface HomeProps {
 
 // Home page path="/home"
 export function Home(props:HomeProps) {  
-  const [isPopupOpen, setPopupOpen]  = useState(false);
-  // user name
-  const [name,         setName]         = useState('');
-  // user password
-  const [password,     setPassword]     = useState('');
-  const [actionType,   setActionType]   = useState<'join' | 'create' | undefined>();
-  const [LobbyCreated, setLobbyCreated] = useState(false);
+  const [isTeacherPopupOpen,  setTeacherPopupOpen]  = useState(false);
+  const [isStudentPopupOpen,  setStudentPopupOpen]  = useState(false);
+  const [name,                setName]              = useState('');
+  const [password,            setPassword]          = useState('');
+  const [actionType,          setActionType]        = useState<'join' | 'create' | undefined>();
+  const [LobbyCreated,        setLobbyCreated]      = useState(false);
 
   const openPopup = (type: 'join' | 'create') => {
     setActionType(type);
-    setPopupOpen(true);
+    if (type == 'join') {setStudentPopupOpen(true);}
+    else {setTeacherPopupOpen(true);}
   };
 
   const closePopup = () => {
-    setPopupOpen(false);
+    setStudentPopupOpen(false);
+    setTeacherPopupOpen(false);
   };
 
   const handleLinkClick = () => {
-    const encodedName = encodeURIComponent(name);
-    window.location.href = `joinlobby?name=${encodedName}`; // jump to joinlobby page
+    if(name){
+      const encodedName = encodeURIComponent(name);
+      window.location.href = `joinlobby?name=${encodedName}`; // jump to joinlobby page
+    }
+
   };
 
   const handleCreateRoomClick = () => {
-    closePopup();
-    const encodedName = encodeURIComponent(name);
-    window.location.href = `createlobby?name=${encodedName}`; // jump to createlobby page
+    if(name){
+      closePopup();
+      const encodedName = encodeURIComponent(name);
+      window.location.href = `createlobby?name=${encodedName}`; // jump to createlobby page
+    }
+    
   };
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,11 +56,8 @@ export function Home(props:HomeProps) {
   const handleNameKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      if (actionType === 'join') {
-        handleLinkClick();
-      } else {
-        handleCreateRoomClick();
-      }
+      if (actionType === 'join') {handleLinkClick();} 
+      else {handleCreateRoomClick();}
     }
   };
 
@@ -66,13 +71,13 @@ export function Home(props:HomeProps) {
 
   return (
     <div className="Home">
+
       <header>
         <img src="logo.jpg" alt="Your Logo" className="logohome" />
       </header>
+
       <main className="centerhome">
-        <h1 className="welcome">
-          Welcome to ChatBot!
-        </h1>
+        <h1 className="welcome">EduChat</h1>
 
           <button className="button" onClick={() => openPopup('create')}>
             Teacher
@@ -84,12 +89,12 @@ export function Home(props:HomeProps) {
             Student
           </button>}
 
-        {isPopupOpen && (
+        {isTeacherPopupOpen && (
         <div>
           <div className="popup-overlay"></div>
           <div className="popup">
 
-            <h2>Login</h2>
+            <h2>Teacher Login</h2>
             <input
               type="text"
               value={name}
@@ -105,6 +110,36 @@ export function Home(props:HomeProps) {
               onKeyPress={handleNameKeyPress}
               placeholder="Enter your password"
             />
+
+            <button onClick={closePopup}>
+              Cancel
+            </button>
+
+            <button onClick={actionType === 'create' ? handleCreateRoomClick : handleLinkClick}>
+              Submit
+            </button>
+            
+          </div>
+        </div>
+        )}
+
+      {isStudentPopupOpen && (
+        <div>
+          <div className="popup-overlay"></div>
+          <div className="popup">
+
+            <h2>Student Login</h2>
+            <input
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+              onKeyPress={handleNameKeyPress}
+              placeholder="Enter your name"
+            />
+
+            <button onClick={closePopup}>
+              Cancel
+            </button>
 
             <button onClick={actionType === 'create' ? handleCreateRoomClick : handleLinkClick}>
               Submit
