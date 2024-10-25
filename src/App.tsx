b6fe0897d9b814a108b23608e26c1b12f733a2c1
Matthew Sah <mattsah2001@gmail.com>
2024-10-24 17:08:42 -0700

    prefix frontend env variables with REACT_APP_

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { Chatroom } from './pages/Chatroom/Chatroom';
 import { Monitor } from './pages/Monitor/Monitor';
 import io from 'socket.io-client';
 
-const SERVER_URL = process.env.SERVER_URL ?? "";
-const socket = io(SERVER_URL);
+const REACT_APP_SERVER_URL = process.env.SERVER_URL ?? "";
+const socket = io(REACT_APP_SERVER_URL);
 
 // "/home" is the homepage
 // Teacher route: CreateLobby->LobbySettings->
